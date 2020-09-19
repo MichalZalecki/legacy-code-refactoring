b3d379f480ee46577aa57f5810e155698b668985
Michal Zalecki <michal@michalzalecki.com>
2020-09-19 22:16:44 +0200

    Add exercise 02

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ You've been asked to change the "Load" button text to "Load users". You also hav
 
     npx parcel exercises/01-seams-react/index.html
     npx jest 01
+
+**02. Sprout Method**
+
+You're dealing with an app that's using a custom framework that also happens to rely on a global state. You have a little time to add a new feature. Your task is to use a Sprout Method and change the app so it greets the user in the currently set language. This way for English the greeting is "Good morning, Bob!", for Czech the greeting is "Dobré ráno, Bob!" and so on.
+
+    npx parcel exercises/02-sprout/index.html
+    npx jest 02
---
+++ exercises/02-sprout/index.js
@@ -0,0 +1,42 @@
+window.myApp = {
+  translations: {
+    en: {
+      greeting: "good morning",
+    },
+    pl: {
+      greeting: "dzień dobry",
+    },
+    ru: {
+      greeting: "доброе утро",
+    },
+    cz: {
+      greeting: "dobré ráno",
+    },
+  },
+  setLanguage(language) {
+    this.language = language;
+  },
+  setUser(user) {
+    this.user = user;
+  },
+  greetings() {
+    return `Hello, ${this.user.firstName}`;
+  },
+};
+
+window.myApp.setUser({ firstName: "Bob" });
+window.myApp.setLanguage("en");
+
+(function render() {
+  window.root.innerHTML = `
+    <div>Current language: ${window.myApp.language}</div>
+    <h1>${window.myApp.greetings()}</h1>
+    <button id="lang">New language</button>
+  `;
+
+  window.lang.addEventListener("click", () => {
+    const langs = Object.keys(window.myApp.translations);
+    window.myApp.setLanguage(langs[Math.floor(Math.random() * langs.length)]);
+    render();
+  });
+})();
