f0b10dbad954921acc1a838219e6f497ad03bd1a
Michal Zalecki <michal@michalzalecki.com>
2020-09-19 18:39:28 +0200

    Create exercise 01-seams-react

---
+++ README.md
@@ -0,0 +1,6 @@
+# legacy-code-refactoring
+
+**01. Identify Seams to break dependencies**
+
+    npx parcel exercises/01-seams-react/index.html
+    npx jest 01
---
+++ exercises/01-seams-react/component.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+// 1. Component is doing too much things (load button, users list, single user)
+// 2. Component can't be unit tested (performs API request)
+// 3. Component is coupled with GitHub API internals (avatar_url)
+
+export function Component() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState(null);
+
+  if (user) {
+    return (
+      <div>
+        <button onClick={() => setUser(null)}>Back</button>
+        <ul>
+          <li>
+            <img src={user.avatar_url} alt="" />
+          </li>
+          <li>
+            <a href={user.html_url} target="_blank" rel="noreferrer noopener">
+              @{user.login}
+            </a>
+          </li>
+          <li>Followers: {user.followers}</li>
+        </ul>
+      </div>
+    );
+  } else if (users.length === 0) {
+    return (
+      <button
+        onClick={() => {
+          axios.get("https://api.github.com/users").then((res) => {
+            setUsers(res.data);
+          });
+        }}
+      >
+        Load
+      </button>
+    );
+  } else {
+    return (
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <button
+              onClick={() => {
+                axios
+                  .get(`https://api.github.com/users/${user.login}`)
+                  .then((res) => {
+                    setUser(res.data);
+                  });
+              }}
+            >
+              @{user.login}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
