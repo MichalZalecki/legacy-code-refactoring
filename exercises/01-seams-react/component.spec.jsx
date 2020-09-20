ba9f967d4eb5aa2e2150e39743e681e921f6aebd
Michal Zalecki <michal@michalzalecki.com>
2020-09-20 12:27:23 +0200

    Use async/await in tests

--- exercises/01-seams-react/component.spec.jsx
+++ exercises/01-seams-react/component.spec.jsx
@@ -5,38 +5,33 @@ import { fetchUser, fetchUsers } from "./UsersRepository";
 
 jest.mock("./UsersRepository");
 
-function delay(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
 function userFixture(user) {
   return { id: 1, login: "Ania", followers: 2, repositories: 91, ...user };
 }
 
 describe("<Component />", () => {
-  it("renders users list", (done) => {
+  it("renders users list and a single user", async () => {
     fetchUsers.mockResolvedValue([
       userFixture(),
       userFixture({ id: 2, login: "Bartek" }),
     ]);
     fetchUser.mockResolvedValue(userFixture({ followers: 3210 }));
+
     const root = render(<Component />);
+
     fireEvent.click(root.getByText("Load users"));
-    waitFor(() => {
+
+    await waitFor(() => {
       expect(root.container.querySelectorAll("li")).toHaveLength(2);
       expect(root.getByText(/Ania/).tagName).toEqual("BUTTON");
       expect(root.getByText(/Bartek/).tagName).toEqual("BUTTON");
-      fireEvent.click(root.getByText(/Ania/));
+    });
 
-      return waitFor(() => {
-        root.getByText("Followers: 3210");
-        root.getByText("Repositories: 91");
-      });
-    }).then(() => {
-      root.debug();
-      done();
+    fireEvent.click(root.getByText(/Ania/));
+
+    await waitFor(() => {
+      root.getByText("Followers: 3210");
+      root.getByText("Repositories: 91");
     });
   });
 });
