ea637bd8617871795ab168b0ce52053f38fb7ee7
Michal Zalecki <michal@michalzalecki.com>
2020-09-20 13:15:59 +0200

    Exercise 01

--- exercises/01-seams-react/component.jsx
+++ exercises/01-seams-react/component.jsx
@@ -1,48 +1,69 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { fetchUser, fetchUsers } from "./UsersRepository";
+import * as UsersRepository from "./UsersRepository";
 
-// 1. Component is doing too much (load button, users list, single user)
-// 2. Component can't be unit tested (performs API request)
-// 3. Component is coupled with GitHub API internals (avatar_url)
+// 1. ✅ Component is doing too much (load button, users list, single user)
+// 2. ✅ Component can't be unit tested (performs API request)
+// 3. ✅ Component is coupled with GitHub API internals (avatar_url)
+
+function LoadButton({ onClick }) {
+  return <button onClick={onClick}>Load users</button>;
+}
+
+function SingleUserView({ onBack, user }) {
+  return (
+    <div>
+      <button onClick={onBack}>Back</button>
+      <ul>
+        <li>
+          <img src={user.avatar_url} alt="" />
+        </li>
+        <li>
+          <a href={user.html_url} target="_blank" rel="noreferrer noopener">
+            @{user.login}
+          </a>
+        </li>
+        <li>{`Followers: ${user.followers}`}</li>
+        <li>{`Repositories: ${user.repositories}`}</li>
+      </ul>
+    </div>
+  );
+}
+
+function UsersList({ users, onSelection }) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <button onClick={() => onSelection(user.login)}>@{user.login}</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export function Component() {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState(null);
 
-  if (user) {
-    return (
-      <div>
-        <button onClick={() => setUser(null)}>Back</button>
-        <ul>
-          <li>
-            <img src={user.avatar_url} alt="" />
-          </li>
-          <li>
-            <a href={user.html_url} target="_blank" rel="noreferrer noopener">
-              @{user.login}
-            </a>
-          </li>
-          <li>{`Followers: ${user.followers}`}</li>
-          <li>{`Repositories: ${user.repositories}`}</li>
-        </ul>
-      </div>
-    );
-  } else if (users.length === 0) {
-    return (
-      <button onClick={() => fetchUsers().then(setUsers)}>Load users</button>
-    );
-  } else {
-    return (
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <button onClick={() => fetchUser().then(setUser)}>
-              @{user.login}
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
+  function resetUser() {
+    setUser(null);
   }
+
+  function selectUser(login) {
+    UsersRepository.fetchUser(login).then(setUser);
+  }
+
+  function loadUsers() {
+    UsersRepository.fetchUsers().then(setUsers);
+  }
+
+  if (user) {
+    return <SingleUserView onBack={resetUser} user={user} />;
+  }
+
+  if (users.length === 0) {
+    return <LoadButton onClick={loadUsers} />;
+  }
+
+  return <UsersList onSelection={selectUser} users={users} />;
 }
--- exercises/01-seams-react/component.spec.jsx
+++ exercises/01-seams-react/component.spec.jsx
@@ -9,29 +9,57 @@ function userFixture(user) {
   return { id: 1, login: "Ania", followers: 2, repositories: 91, ...user };
 }
 
+async function itLoadsUsersList(root) {
+  fireEvent.click(root.getByText("Load users"));
+
+  await waitFor(() => {
+    expect(root.container.querySelectorAll("li")).toHaveLength(2);
+    expect(root.getByText(/Ania/).tagName).toEqual("BUTTON");
+    expect(root.getByText(/Bartek/).tagName).toEqual("BUTTON");
+  });
+}
+
+const USER_ANIA = userFixture({ followers: 3210 });
+const USER_BARTEK = userFixture({ id: 2, login: "Bartek", followers: 12 });
+
 describe("<Component />", () => {
+  beforeEach(() => {
+    fetchUsers.mockResolvedValue([USER_ANIA, USER_BARTEK]);
+  });
+
   it("renders users list and a single user", async () => {
-    fetchUsers.mockResolvedValue([
-      userFixture(),
-      userFixture({ id: 2, login: "Bartek" }),
-    ]);
-    fetchUser.mockResolvedValue(userFixture({ followers: 3210 }));
+    fetchUser.mockResolvedValue(USER_ANIA);
 
     const root = render(<Component />);
 
-    fireEvent.click(root.getByText("Load users"));
-
-    await waitFor(() => {
-      expect(root.container.querySelectorAll("li")).toHaveLength(2);
-      expect(root.getByText(/Ania/).tagName).toEqual("BUTTON");
-      expect(root.getByText(/Bartek/).tagName).toEqual("BUTTON");
-    });
+    await itLoadsUsersList(root);
 
     fireEvent.click(root.getByText(/Ania/));
 
     await waitFor(() => {
       root.getByText("Followers: 3210");
       root.getByText("Repositories: 91");
+    });
+  });
+
+  it("allows to back to users list from the single user view", async () => {
+    fetchUser.mockResolvedValue(USER_BARTEK);
+
+    const root = render(<Component />);
+
+    await itLoadsUsersList(root);
+
+    fireEvent.click(root.getByText(/Bartek/));
+
+    await waitFor(() => {
+      expect(fetchUser).toHaveBeenCalledWith("Bartek");
+      root.getByText("Followers: 12");
+    });
+
+    fireEvent.click(root.getByText("Back"));
+
+    await waitFor(() => {
+      expect(root.container.querySelectorAll("li")).toHaveLength(2);
     });
   });
 });
